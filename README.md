b346c749a0628714ee2626e9a8bea41f21a5cfd2
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2022-02-06 21:10:04 +0200

    feat: second video & modify cod

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # V-Minin-04-board-sources
 
 [demo](https://skirnevskyialeksandr.github.io/V-Minin-04-board-sources/)  
-[video](https://vladilen.ru/marathon/board)
+[video](https://vladilen.ru/marathon/board)  
+[video](https://www.youtube.com/watch?v=gX3Ev1ATxfI)
--- app.js
+++ app.js
@@ -7,24 +7,26 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     square.classList.add('square')
     board.append(square)
 
-    square.addEventListener('mouseover', () => { setColor(square) }
+    square.addEventListener('mouseover', setColor
     )
 
-    square.addEventListener('mouseleave', (event) => {
-        event.target.style.backgroundColor = "#1d1d1d"
-        event.target.style.boxShadow = '0 0 2px #000'
-    })
+    square.addEventListener('mouseleave', removeColo)
 }
 
 
-function setColor(elem) {
+function setColor(event) {
     const color = randomColor()
-    elem.style.backgroundColor = color
-    elem.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`
+    event.target.style.backgroundColor = color
+    event.target.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`
+}
+
+function removeColo(event) {
+    const element = event.target
+    element.style.backgroundColor = "#1d1d1d"
+    element.style.boxShadow = '0 0 2px #000'
 }
 
 function randomColor() {
-    const index = Math.floor(Math.random() * arrayColor.length)
-    return arrayColor[index]
+    return arrayColor[Math.floor(Math.random() * arrayColor.length)]
 }
 
